f1747a7b389a00ebb09ee79df0b9dcaae8544578
Eueopia <dreameutopia@163.com>
2024-10-19 22:13:38 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -14,34 +14,7 @@ const Footer = ({ siteInfo }) => {
 
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
-      <SocialButton />
-
-      <div className='flex justify-center'>
-        <div>
-          <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-          <a
-            href={siteConfig('LINK')}
-            className='underline font-bold text-gray-500 dark:text-gray-300 '>
-            {siteConfig('AUTHOR')}
-          </a>
-          .<br />
-        </div>
-        © {`${copyrightDate}`}
-      </div>
-
-      {siteConfig('BEI_AN') && (
-        <>
-          <i className='fas fa-shield-alt' />{' '}
-          <a href='https://beian.miit.gov.cn/' className='mr-2'>
-            {siteConfig('BEI_AN')}
-          </a>
-          <br />
-        </>
-      )}
-
-      
-      {/* SEO title */}
-      <h1 className='pt-1 hidden'>{siteConfig('TITLE')}</h1>
+     
     </footer>
   )
 }
